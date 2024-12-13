e146827c7f43969b09cb964dd16bcfe581c20230
Zeuxis Lo <seekstudio@gmail.com>
2024-12-13 21:22:40 +0800

    add check to ensure the `$CONFIG_PATH` variable already set when using the `alldir` linker

--- _utils/linkers/alldir.sh
+++ _utils/linkers/alldir.sh
@@ -1,4 +1,9 @@
 function link {
+    if [[ -z "${CONFIG_PATH+any}" ]]; then
+        echo 'Please set the CONFIG_PATH variable first when using the `alldir` linker'
+        exit 1
+    fi
+
     mkdir -p $CONFIG_PATH
 
     for file_or_dir_path in `ls -d $INSTALLER_PATH/??* $INSTALLER_PATH/.??* | grep -vE '.DS_Store|README.md|main.sh$'`; do
@@ -12,6 +17,11 @@ function link {
 }
 
 function unlink {
+    if [[ -z "${CONFIG_PATH+any}" ]]; then
+        echo 'Please set the CONFIG_PATH variable first when using the `alldir` linker'
+        exit 1
+    fi
+
     for file_or_dir_path in `ls -d $INSTALLER_PATH/??* $INSTALLER_PATH/.??* | grep -vE '.DS_Store|README.md|main.sh$'`; do
         echo "Unlink from $file_or_dir_path"
 
