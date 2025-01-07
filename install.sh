0e67dd32ae69e5715e04e4f174bc5a34f0843caf
Zeuxis Lo <seekstudio@gmail.com>
2025-01-07 21:13:58 +0800

    install: add helix config directory

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 HOME_PATH=$HOME
 CURRENT_PATH=$PWD
-CONFIG_DIRS=(ghostty git screen starship vim wezterm zed zellij zsh)
+CONFIG_DIRS=(ghostty git helix screen starship vim wezterm zed zellij zsh)
 
 error_status=0
 
