438c32dad9c289ea63660a576959c7356b159e88
Zeuxis Lo <seekstudio@gmail.com>
2025-01-13 19:14:31 +0800

    install: add btop config directory

---
+++ btop/main.sh
@@ -0,0 +1,19 @@
+source $PWD/_utils/variables.sh
+source $PWD/_utils/functions.sh
+source $PWD/_utils/linkers/alldir.sh
+
+# set install directory path
+INSTALLER_PATH=$CURRENT_PATH/btop
+
+# set self linked directory path and files, directory
+CONFIG_PATH=$HOME_PATH/.config/btop
+
+function install {
+    link
+}
+
+function uninstall {
+    unlink
+}
+
+source $PWD/_utils/commands.sh
