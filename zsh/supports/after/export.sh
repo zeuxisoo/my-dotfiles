cc392b524b0f51534c2cf26e1298388865c9865e
Zeuxis Lo <seekstudio@gmail.com>
2025-02-24 20:30:48 +0800

    zsh: load fnm if exists

--- zsh/supports/after/export.sh
+++ zsh/supports/after/export.sh
@@ -4,3 +4,8 @@ export HOMEBREW_NO_AUTO_UPDATE=1
 
 # starship
 export STARSHIP_CONFIG=$HOME/.config/starship/starship.toml
+
+# fnm
+if cmd_exists "fnm"; then
+    eval "$(fnm env --use-on-cd --shell zsh)"
+fi
