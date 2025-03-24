1e9dbda83588fee998744fc95999c2c1050d9999
Zeuxis Lo <seekstudio@gmail.com>
2025-03-24 18:41:51 +0800

    zsh: enable shell integration, add preview scroll binding and style to fzf default options

--- zsh/supports/after/export.sh
+++ zsh/supports/after/export.sh
@@ -9,12 +9,24 @@ export STARSHIP_CONFIG=$HOME/.config/starship/starship.toml
 export _ZO_DATA_DIR=$HOME/.config/zoxide
 
 # fzf
-export FZF_DEFAULT_OPTS=" \
-    --color=bg+:#363a4f,bg:#24273a,spinner:#f4dbd6,hl:#ed8796 \
-    --color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6 \
-    --color=marker:#b7bdf8,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796 \
-    --color=selected-bg:#494d64 \
-    --color=border:#363a4f,label:#cad3f5"
+if cmd_exists "fzf"; then
+    # ctrl+b/f -> up&down in preview
+    export FZF_DEFAULT_OPTS=" \
+        --style full \
+        --preview 'fzf-preview.sh {}' \
+        --bind 'focus:transform-header:file --brief {}' \
+        --bind 'ctrl-b:preview-up,ctrl-f:preview-down' \
+        --color=bg+:#363a4f,bg:#24273a,spinner:#f4dbd6,hl:#ed8796 \
+        --color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6 \
+        --color=marker:#b7bdf8,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796 \
+        --color=selected-bg:#494d64 \
+        --color=border:#363a4f,label:#cad3f5"
+    # shell integration, in terminal like:
+    # - type `vim` > press `ctrl-t` > pick file to complete command
+    # - press `ctrl-r` pick command from history
+    # - press `alt-c` change to selected directory
+    source <(fzf --zsh)
+fi
 
 # fnm
 if cmd_exists "fnm"; then
